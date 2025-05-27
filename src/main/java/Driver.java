99bca9f500b7506e1ba17992e94f1f5f13ea80bd
Muhammad Agam Nasywaan <112670327+agamnsy@users.noreply.github.com>
2025-05-27 10:44:47 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -15,6 +15,18 @@ public class Driver {
 		counter.decrement();
 		
 		System.out.println("Current count:" + counter.getCount());
+
+		counter.increaseBy(5);
+
+		system.out.println("Current count:" + counter.getCount());
+
+		counter.decreaseBy(2);
+
+		system.out.println("Current count:" + counter.getCount());
+
+		counter.multiplyBy(6);
+
+		system.out.println("Current count:" + counter.getCount());
 		
 	}
 
